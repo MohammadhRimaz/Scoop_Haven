e44d7c2c450c389802461c40fbaea4b37c7b66a2
MohammadhRimaz <mohammadhrimaz@gmail.com>
2024-11-13 20:37:38 +0530

    Changes made in auth/route

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -107,19 +107,17 @@ export const authOptions = {
   },
 };
 
-export async function isAdmin() {
+async function isAdmin() {
   const session = await getServerSession(authOptions);
   const userEmail = session?.user?.email;
   if (!userEmail) {
     return false;
   }
   const userInfo = await UserInfo.findOne({ email: userEmail });
-  if (!userInfo) {
-    return false;
-  }
-  return userInfo.admin;
+  return !!userInfo?.admin;
 }
 
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
+export { isAdmin };
